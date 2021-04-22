38e44465fb185ff4d9bd541a22f0e12740d8b517
Adrian Esquivel <adrian@kapiwebdev.com>
2021-04-22 08:30:48 -0600

    s3.js: Adding deleteFile(), moveFile(). Fixing list() query.

--- s3.js
+++ s3.js
@@ -1,10 +1,11 @@
 const AWS = require('aws-sdk');
-const { S3Client, ListObjectsCommand, GetObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, ListObjectsCommand, GetObjectCommand, PutObjectCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 
 module.exports = class aws_s3 {
     
     constructor(bucket, region=process.env.AWS_REGION) {
         this.region = region;
+        
         this.bucket = bucket;
         
         this.s3 = new S3Client({ region: this.region });
@@ -16,7 +17,7 @@ module.exports = class aws_s3 {
         
         if(dir) {
             dir = dir.replace(/^\/|\/$/g, '');
-            options.Marker = dir;
+            options.Prefix = dir;
         }
         
         try {
@@ -24,10 +25,10 @@ module.exports = class aws_s3 {
             let result = [];
             
             for(let i in data.Contents) {
-                let filename = data.Contents[i].Key.replace(dir + '/', '');
+                let fileName = filename(data.Contents[i].Key);
                 
-                if(filename) {
-                    result.push(filename);
+                if(fileName) {
+                    result.push(fileName);
                 }
             }
             
@@ -35,6 +36,29 @@ module.exports = class aws_s3 {
             
         } catch (err) {
             console.log("CALLERR aws_s3.list(): ", err);
+        }
+    }
+    
+    
+    //Absolute path with filename for both params
+    async moveFile(file, newFile) {
+        let fileContents = await this.getFile(file);
+        
+        await this.putFile(newFile, fileContents);
+        
+        await this.deleteFile(file);
+    }
+    
+    
+    
+    async deleteFile(file) {
+        let options = this.initOptions()
+        options.Key = file;
+        
+        try {
+            return await this.s3.send(new DeleteObjectCommand(options));            
+        } catch (err) {
+            console.log("Error", err);
         }
     }
     
@@ -59,10 +83,10 @@ module.exports = class aws_s3 {
     }
     
     
-    async putFile(filename, file_contents) {
+    async putFile(filename, fileContents) {
         let options = this.initOptions();
         options.Key = filename;
-        options.Body = file_contents;
+        options.Body = fileContents;
         
         try {
             const data = await this.s3.send(new PutObjectCommand(options));
